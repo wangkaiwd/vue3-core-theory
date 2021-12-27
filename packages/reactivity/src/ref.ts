8f66a10ce7886ca7b071006f9d7bb2e9dd13dbe2
wangkaiwd <1162486176@qq.com>
2021-12-27 12:13:10 +0800

    feat: implement toRef and toRefs

--- packages/reactivity/src/ref.ts
+++ packages/reactivity/src/ref.ts
@@ -1,4 +1,4 @@
-import { hasChanged, isObject } from '@sppk/shared';
+import { hasChanged, isArray, isObject } from '@sppk/shared';
 import { reactive } from './reactive';
 import { track, trigger } from './effect';
 
@@ -10,6 +10,7 @@ class RefImpl {
   public _value;
   public _shallow;
   public _rawValue;
+  public __v_isRef = true;
 
   constructor (rawValue, shallow: boolean) {
     this._rawValue = rawValue;
@@ -40,4 +41,31 @@ export const ref = (value) => {
 };
 export const shallowRef = (value) => {
   return createRef(value, true);
+};
+
+class ObjectRefImpl {
+  constructor (public target, public prop) {}
+
+  // this will trigger reactive proxy get/set trap
+  get value () {
+    return this.target[this.prop];
+  }
+
+  set value (newValue) {
+    this.target[this.prop] = newValue;
+  }
+}
+
+export const toRef = (target, prop) => {
+  return new ObjectRefImpl(target, prop);
+};
+
+// execute toRef for all target properties and return a new array or object
+export const toRefs = (target) => {
+  const result = isArray(target) ? [] : {};
+  const keys = Object.keys(target);
+  keys.forEach(key => {
+    result[key] = toRef(target, key);
+  });
+  return result;
 };
